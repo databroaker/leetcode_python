765f882f2a6d817ecead9c92caa61a0afdcc8753
Myk3le <myk3le@proton.me>
2025-08-07 19:00:30 -0700

    Check if two string arrays are equivalent

---
+++ array_strings_same.py
@@ -0,0 +1,5 @@
+class Solution(object):
+    def arrayStringsAreEqual(self, word1, word2):
+        return "".join(word1) == "".join(word2)
+
+print(Solution().arrayStringsAreEqual(["ab", "c"], ["a", "bc"]))
